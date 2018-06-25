d6b7991eb1ce57819479e669269a23209f335eae
nadunD99 <39294235+nadunD99@users.noreply.github.com>
2018-06-25 18:48:02 +0530

    Update Form1.Designer.cs

--- Trend Line Design/Project/Form1.Designer.cs
+++ Trend Line Design/Project/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// This required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
@@ -23,7 +23,7 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
+        /// This required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
